42996c831ba22c7943ded503e3b00cbfa4c2d9ba
Afolabi Oluwatosin <tafolabi009@gmail.com>
2025-06-20 10:56:50 +0100

    feat: add Google Analytics and Microsoft Clarity scripts to layout for enhanced tracking and user insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,26 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         {/* Ahrefs Web Analytics */}
         <script src="https://analytics.ahrefs.com/analytics.js" data-key="asrHwBiwtOQ8Q+sudjXgOA" async></script>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GA_MEASUREMENT_ID');
+          `,
+        }} />
+        {/* Microsoft Clarity */}
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/CLARITY_PROJECT_ID";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "CLARITY_PROJECT_ID");
+          `
+        }} />
       </head>
       <body className={`${inter.variable} ${montserrat.variable} font-sans antialiased bg-background text-foreground`}>
         <Providers>
